561dd1f2dba7201941468f6f3a3f256ed0cdd21a
jrfm <moesch.jonas@protonmail.com>
2023-05-24 23:39:11 +0200

    add basic projects section

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -2,25 +2,34 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
 
 export default function Porfolio() {
   return (
     <>
-      <Container>
-        {/* about me */}
-        <Row xs={1} lg={2}>
-          <Col className="mt-5 ">
-            <div className="d-flex justify-content-center">
+      {/* about me */}
+      <Container className="mt-5 ">
+        <Row
+          id="about-me"
+          xs={1}
+          lg={2}
+          className="d-flex justify-content-center"
+        >
+          <Col className="d-flex justify-content-center">
+            <div>
               <Image
-                style={{ height: "700px", objectFit: "cover" }}
+                style={{
+                  width: "100%",
+                  objectFit: "cover",
+                }}
                 src="images/mountain_small.jpg"
                 rounded
                 fluid
               />
             </div>
           </Col>
-          <Col className="d-flex align-items-center">
-            <div className="mt-4 ">
+          <Col className="d-flex align-items-center mt-3">
+            <div>
               <h2>About Me</h2>
               <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
@@ -34,6 +43,47 @@ export default function Porfolio() {
                 et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
                 est Lorem ipsum dolor sit amet.
               </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* projects */}
+      <Container className="mt-5 d-flex justify-content-center">
+        <Row id="projects" xs={1} lg={3}>
+          <Col>
+            <div>
+              <h2>Projects</h2>
+              <Card style={{ width: "25rem", marginTop: "2rem" }}>
+                <Card.Img variant="top" src="images/todo.jpg" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: "25rem", marginTop: "2rem" }}>
+                <Card.Img variant="top" src="images/todo.jpg" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: "25rem", marginTop: "2rem" }}>
+                <Card.Img variant="top" src="images/todo.jpg" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
             </div>
           </Col>
         </Row>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import Nav from "react-bootstrap/Nav";
+
 import Navbar from "react-bootstrap/Navbar";
 
 function Navigation() {
@@ -6,8 +7,8 @@ function Navigation() {
     <>
       <Navbar sticky="top" bg="secondary" variant="dark">
         <Nav className="me-auto">
-          <Nav.Link>About Me</Nav.Link>
-          <Nav.Link>Projects</Nav.Link>
+          <Nav.Link href="#about-me">About Me</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
           <Nav.Link>Contact</Nav.Link>
         </Nav>
       </Navbar>
